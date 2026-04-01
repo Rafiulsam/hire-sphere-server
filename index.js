1121907b792e1f5fd2bbd9e034351190c9ff6f24
Rafiul Sam <rafiulsam69@gmail.com>
2026-04-02 03:40:19 +0600

    Application get api implemented

--- index.js
+++ index.js
@@ -31,31 +31,55 @@ async function run() {
 
         //jobs collection
         const jobsCollection = client.db("HireSphere").collection("jobs");
+
         //applications collection
         const applicationsCollection = client.db("HireSphere").collection("applications");
 
+        // Jobs APIs
         // API to get all jobs
         app.get('/jobs', async (req, res) => {
             const cursor = jobsCollection.find();
             const result = await cursor.toArray();
             res.send(result);
-        }) 
-        
+        })
+
         // API to get a single job by ID
         app.get('/jobs/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) };    
+            const query = { _id: new ObjectId(id) };
             const result = await jobsCollection.findOne(query);
             res.send(result);
         });
 
+        //Applications APIs
+        //Api to get all applications
+        app.get('/applications', async (req, res) => {
+            const applicant_email = req.query.email;
+            const query = {
+                applicantEmail: applicant_email
+            }
+            const result =await applicationsCollection.find(query).toArray();
+
+            // Not good practice 
+            for (const application of result) {
+                const jobId = application.jobId
+                const jobQuery = {_id: new ObjectId(jobId)} 
+                const job = await jobsCollection.findOne(jobQuery)
+                application.company = job.company
+                application.title = job.title
+                application.company_logo = job.company_logo
+                application.location = job.location
+            }
+           res.send(result);
+        })
+
         // API to post a application
-        app.post('/applications', async (req, res) => { 
+        app.post('/applications', async (req, res) => {
             const application = req.body;
             const result = await applicationsCollection.insertOne(application);
             res.send(result);
         })
-        
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
